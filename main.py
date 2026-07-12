e6563d6a2b08465b4bcca192b0e4cb5832804ccb
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-12 09:04:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 from analysis import analyze_market
 
-TOKEN = "8858152810:AAFy1Pe1iE_OYImkME2xwD0UreV9jhFh88s"
+TOKEN = "8858152810:AAHvXkD78ybAMb9Tmf9_6bYrhcUnqEPt2zw"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
