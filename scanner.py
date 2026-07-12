593cf88bcfa45b27742fe381562f66b851f4e2d1
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-12 16:58:44 +0100

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,44 @@
+import asyncio
+from datetime import datetime
 
+from analysis import analyze_market
+
+
+PAIRS = [
+    "XAUUSD",
+    "EURUSD",
+    "GBPUSD"
+]
+
+
+async def start_scanner():
+
+    print("Market scanner started...")
+
+
+    while True:
+
+        for pair in PAIRS:
+
+            try:
+                result = analyze_market(pair)
+
+                print(
+                    f"\n{datetime.now()}\n"
+                    f"{result}"
+                )
+
+
+                # Only display possible signals
+                if "BUY 🟢" in result or "SELL 🔴" in result:
+                    print("🚨 SIGNAL FOUND")
+
+
+            except Exception as e:
+                print(
+                    f"Scanner error {pair}: {e}"
+                )
+
+
+        # Check every 5 minutes
+        await asyncio.sleep(300)
