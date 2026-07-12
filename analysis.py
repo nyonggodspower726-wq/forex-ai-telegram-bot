ef60c330712d693d61f75b440e676c68c06bd438
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-12 10:14:55 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,20 +1,16 @@
 import requests
 
-API_KEY = "aba787bf68ba4008b359f34229fdbc29"
+API_KEY = "YOUR_TWELVE_DATA_API_KEY"
 
 def analyze_market(symbol):
-    symbols = {
-        "BTCUSD": "BTC/USD",
-        "ETHUSD": "ETH/USD",
-        "XAUUSD": "XAU/USD",
-    }
 
-    symbol = symbol.upper()
-
-    if symbol in symbols:
-        symbol = symbols[symbol]
-
-    url = f"https://api.twelvedata.com/price?symbol={symbol}&apikey={API_KEY}"
+    url = (
+        f"https://api.twelvedata.com/time_series"
+        f"?symbol={symbol}"
+        f"&interval=5min"
+        f"&outputsize=10"
+        f"&apikey={API_KEY}"
+    )
 
     response = requests.get(url)
     data = response.json()
