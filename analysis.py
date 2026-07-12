fa5f8fa9a1c165e68ad03b66e100dc802f9f9f59
nyonggodspower726-wq <nyonggodspower726@gmail.com>
2026-07-12 10:48:00 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,8 +1,17 @@
 import requests
 
-API_KEY = "aba787bf68ba4008b359f34229fdbc29"
+API_KEY = "YOUR_TWELVE_DATA_KEY"
+
 
 def analyze_market(symbol):
+
+    symbols = {
+        "XAUUSD": "XAU/USD",
+        "BTCUSD": "BTC/USD",
+        "ETHUSD": "ETH/USD"
+    }
+
+    symbol = symbols.get(symbol.upper(), symbol)
 
     url = (
         f"https://api.twelvedata.com/time_series"
@@ -15,4 +24,23 @@ def analyze_market(symbol):
     response = requests.get(url)
     data = response.json()
 
-    return "TESTING NEW STRATEGY FILE"
+    if "values" not in data:
+        return f"❌ Data error:\n{data}"
+
+    candles = data["values"]
+
+    latest = candles[0]
+
+    return f"""
+📊 PipsPilot AI
+
+Symbol: {symbol}
+
+Latest Candle:
+Open: {latest['open']}
+High: {latest['high']}
+Low: {latest['low']}
+Close: {latest['close']}
+
+Candle data connected ✅
+"""
